b8aeeab416d3e5aa5596815cc952d46845f3fded
cmskorcz <chaseskorey@icloud.com>
2022-08-30 17:45:06 -0400

    added comment schema for events

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,4 +1,31 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const formatDate = require('../utils/format-date');
+
+const CommentSchema = new Schema({
+  commentId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId()
+  },
+  commentBody: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdAtVal => formatDate(createdAtVal)
+  }
+},
+{
+  toJSON: {
+    getters: true
+  },
+  id: false
+})
 
 const EventSchema = new Schema({
   title: {
@@ -21,7 +48,8 @@ const EventSchema = new Schema({
   description: {
     type: String,
     required: true
-  }
+  },
+  comments: [CommentSchema]
 })
 
 const Event = model('Event', EventSchema);
