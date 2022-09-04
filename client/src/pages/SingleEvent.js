d2fc161877c96b5a2e7430145edaf9428bd2221a
Nick Earl <nick.r.earl@gmail.com>
2022-09-04 19:52:30 -0400

    single event page get event

--- client/src/pages/SingleEvent.js
+++ client/src/pages/SingleEvent.js
@@ -1,9 +1,27 @@
-
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 
 const EventPage = (props) => {
+  const [ event, setEvent ] = useState({})
+
+  let { id } = useParams()
+
+  const getEvents = async () => {
+      const query = await fetch( `http://localhost:3001/api/event/${id}`)
+      const dbEventData = await query.json()
+      console.log(dbEventData);
+      setEvent(dbEventData)
+    }
+
+  useEffect( ()=> {
+    getEvents()
+  }, [])
+
   return (
-    <div>
-      <h1>This is an Event page</h1>
+    <div className="text-center">
+      <h1>{event.title}</h1>
+      <h3>{event.date} in {event.state}</h3>
+      <p>{event.description}</p>
     </div>
   )
 }
