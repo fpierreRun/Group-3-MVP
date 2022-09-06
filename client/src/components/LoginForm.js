e934005c67b48224df1288704b0de295d11728e3
Dakotah Ulrath <dulrath@live.com>
2022-09-05 22:39:01 -0400

    login and signup working, need to do the backend, and auth

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -67,6 +67,7 @@ const LoginForm = () => {
           Submit
         </Button>
       </Form>
+      
     </>
   )
 }
--- client/src/pages/LoginSingup.js
+++ client/src/pages/LoginSingup.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, } from "react-router-dom"
 import LoginForm from "../components/LoginForm"
-import Signup from './Signup'
+import SignupPage from './Signup'
+import { Container } from "react-bootstrap"
 
 const LoginPage = (props) => {
   return (
